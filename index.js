322008a4636f3c101ad389a528f89df0a126478a
Pushpendra Rajput <dishirajput7@email.com>
2024-07-07 17:15:54 +0530

    newfunction

--- index.js
+++ index.js
@@ -4029,7 +4029,7 @@ app.get('/addResellerMember', async (req, res) => {
     console.log(`User with storeId ${sponsorId} not found`);
     return res.status(405).json({ error: `User with storeId ${sponsorId} not found` });
   }
-  if(isAddedMember){
+  if(!isAddedMember){
     console.log(`You are not allowed to invite any user as you're not a reseller member!`);
     return res.status(404).json({error : `You are not allowed to invite any user as you're not a reseller member!`});
   };
